b5c5a82a1a6dcf2b548d221acf15095acf2469c4
eriollee <eriollee@163.com>
2018-01-05 16:34:47 +0800

    布局优化

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,29 +1,43 @@
 <template>
   <section>
-    <mu-appbar title="图像识别平台">
+    <mu-appbar title="图像识别平台" :zDepth="2">
+      <mu-icon value="settings_input_svideo" color="#0091ea" slot="left">
+      </mu-icon>
     </mu-appbar>
-    <mu-row >
-      <mu-col width="27" tablet="27" desktop="27" >
+    <mu-row :column="2">
+      <mu-col width="25" tablet="25" desktop="25" class="row-left ">
         <mu-list class="list-menu">
-          <mu-list-item title="搜索">
-            <mu-icon value="search" slot="right">
+          <mu-list-item title="搜索" class="menu-active">
+            <mu-icon value="av_timer" slot="left">
             </mu-icon>
           </mu-list-item>
           <mu-list-item title="图片训练库" :toggleNested="true">
-            <mu-icon value="menu" slot="left">
+            <mu-icon value="art_track" slot="left">
             </mu-icon>
             <mu-list-item title="服装" slot="nested">
-              <mu-icon value="shopping_basket" slot="left">
+              <mu-icon value="border_color" slot="left">
+              </mu-icon>
+            </mu-list-item>
+            <mu-list-item title="手机" slot="nested">
+              <mu-icon value="folder" slot="left">
               </mu-icon>
             </mu-list-item>
           </mu-list-item>
-          <mu-list-item>
-          </mu-list-item>
         </mu-list>
+      </mu-col>
+      <mu-col width="75" tablet="75" desktop="75">
+        <mu-row :column="1">
+          <mu-col width="100" tablet="100" desktop="100">
+            <mu-sub-header>
+              仪表盘
+            </mu-sub-header>
+          </mu-col>
+        </mu-row>
       </mu-col>
     </mu-row>
   </section>
 </template>
+
 <style scoped>
   @import "../css/index.css";
 </style>
